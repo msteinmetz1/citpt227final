456b8dc587a0f54b6a43e8dc5e130a9150be4c1b
msteinmetz1 <54378572+msteinmetz1@users.noreply.github.com>
2019-12-10 17:32:08 -0700

    Add files via upload

---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,20 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab1Page } from './tab1.page';
+
+import { NgCalendarModule  } from 'ionic2-calendar';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+   NgCalendarModule
+],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {}
